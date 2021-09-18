2fc604f50977ef08a2d339bc9fbb3312485a7d2f
Ale ∴ <ale@alexvko.com>
2021-09-18 20:53:11 -0300

    Generage boilerplate

---
+++ lib/fast_ci.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "fast_ci/version"
+
+module FastCi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/fast_ci/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module FastCi
+  VERSION = "0.1.0"
+end
